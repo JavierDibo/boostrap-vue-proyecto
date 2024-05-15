1a21522914763051dfa90320bb8ecb871c4ee817
Javier Francisco Dibo Gómez <jfdg0002@red.ujaen.es>
2024-05-15 12:46:27 +0200

    Form Date

---
+++ src/components/FormDateDemo.vue
@@ -0,0 +1,21 @@
+<!-- FormDateDemo.vue -->
+<template>
+  <div>
+    <h2>Form Datepicker</h2>
+    <div>
+      <label for="example-datepicker">Choose a date</label>
+      <b-form-datepicker id="example-datepicker" v-model="selectedDate" class="mb-2"></b-form-datepicker>
+      <p>Selected date: {{ selectedDate }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedDate: ''
+    }
+  }
+}
+</script>
--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -5,6 +5,7 @@
       <h1 class="mb-4">Forms Components</h1>
       <FormDemo />
       <FormCheckboxDemo />
+      <FormDateDemo />
     </div>
   </b-container>
 </template>
@@ -14,8 +15,10 @@
 
 import FormDemo from "@/components/FormDemo.vue";
 import FormCheckboxDemo from "@/components/FormCheckboxDemo.vue";
+import FormDateDemo from "@/components/FormDateDemo.vue";
 export default {
   components: {
+    FormDateDemo,
     FormCheckboxDemo,
     FormDemo,
   },
